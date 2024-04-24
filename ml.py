aea8baa248020022dc716a149278da8b9c15a7da
Naresh Kumar <nkk124421@gmail.com>
2024-04-24 18:57:10 +0530

    change

--- ml.py
+++ ml.py
@@ -1,19 +1,22 @@
-import pandas as pd
-
-newDf=pd.read_csv("./Ready_to_use.csv")
-
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-cv= CountVectorizer(max_features=5000,stop_words="english")
-
-vectors=cv.fit_transform(newDf["tags"])
-
-from sklearn.metrics.pairwise import cosine_similarity
-similarity=cosine_similarity(vectors)
 
 
 def convertIntoObject():
+    
+    print("******************")
+    import pandas as pd
+
+    newDf=pd.read_csv("./Ready_to_use.csv")
+
+
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    cv= CountVectorizer(max_features=5000,stop_words="english")
+
+    vectors=cv.fit_transform(newDf["tags"])
+
+    from sklearn.metrics.pairwise import cosine_similarity
+    similarity=cosine_similarity(vectors)
+
     import pickle
     pickle.dump(newDf,open("movies.pkl","wb"))
     pickle.dump(similarity,open("similarity.pkl","wb"))
