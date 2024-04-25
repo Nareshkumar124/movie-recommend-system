3ca4c68b01554e479496f3f1316255f30958c3e6
Naresh Kumar <nkk124421@gmail.com>
2024-04-25 13:51:46 +0530

    change into class

--- app.py
+++ app.py
@@ -1,53 +1,46 @@
 import streamlit as st
 import pickle
-import requests
-# import pandas as pd
 import os
 
-if not (os.path.isfile("movies.pkl") and  os.path.isfile("movies.pkl")):
-    from ml import convertIntoObject
-    convertIntoObject()
+
+class Recommed:
+    def __init__(self) -> None:
+        if not (os.path.isfile("movies.pkl") and  os.path.isfile("movies.pkl")):
+            from ml import convertIntoObject
+            convertIntoObject()
+        self.load()
     
+    def load(self):
+        self.moviesList=pickle.load(open("movies.pkl","rb"))
+        self.similarity=pickle.load(open("similarity.pkl","rb"))
+        
+    @property
+    def titiles(self):
+        return self.moviesList["title"].values
+        
+    def recommend(self,movie:str):
+        movieIndex=self.moviesList[self.moviesList["title"]==movie].index[0]
+        distances=self.similarity[movieIndex]
+        sort=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1]) 
+        topFive=sort[1:6] 
+        topFiveMoviesDf=self.moviesList.iloc[[index[0] for index in topFive]]
+
+        return topFiveMoviesDf['title']
 
 
-url=""
-imagePath:str=""
-moviesList=pickle.load(open("movies.pkl","rb"))
-similarity=pickle.load(open("similarity.pkl","rb"))
-titles=moviesList["title"].values
-
-def fetchPoster(id):
-    formatedUrl:str=url.format(id)
-    response=requests.get(formatedUrl)
-    data=response.json()
-    return f"{imagePath}{data['poster_path']}"
-    # return "Under work"
 
 
-def recommend(movie:str):
-    movieIndex=moviesList[moviesList["title"]==movie].index[0]
-    distances=similarity[movieIndex]
-    sort=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1]) 
-    topFive=sort[1:10] 
-    # use enumerate to maintain the index and sort baed on index two value
-    topFiveMoviesDf=moviesList.iloc[[index[0] for index in topFive]]
 
-
-    movies=[]
-    for _,movie in topFiveMoviesDf.iterrows():
-        # posterPath=fetchPoster(movie.get("id"))
-        movies.append(movie["title"])
-
-    return movies
+object=Recommed()
 
 st.title("Movie Recommender System")
 
 SelectedMovieName = st.selectbox(
     'Enter the name of the movie',
-    titles)
+    object.titiles)
 
 if st.button("Recommend",type="primary"):
-    recommendMovies=recommend(SelectedMovieName)
+    recommendMovies=object.recommend(SelectedMovieName)
     for movie in recommendMovies:
         st.write(f"### {movie}")
         st.divider()
--- ml.py
+++ ml.py
@@ -1,5 +1,3 @@
-
-
 def convertIntoObject():
     
     print("******************")
